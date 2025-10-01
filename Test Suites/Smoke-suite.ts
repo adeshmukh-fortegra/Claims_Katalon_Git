f0db2cd0bb74d9e253c757cc114d13d8113b56d9
adeshmukh <adeshmukh@ADESHMUKH11.priv.life-south.com>
2025-10-01 10:37:38 -0400

    First change -10/02/2025

---
+++ Test Suites/Smoke-suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke-suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8d74eb3f-d24d-480e-9ea2-b07fad06a6ad</testSuiteGuid>
+</TestSuiteEntity>
